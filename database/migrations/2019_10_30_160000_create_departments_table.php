6f3d28a82b7de5f07b97746f5538bc66985eb064
Pavel Prokopenko <nipplees8@gmail.com>
2019-10-30 19:02:08 +0300

    added department model with resourse controller

---
+++ database/migrations/2019_10_30_160000_create_departments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->integer('amount')->default(0);
+            $table->double('max_salary',15,2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('departments',function(Blueprint $table){
+            $table->dropUnique(['name']);
+        });
+        Schema::dropIfExists('departments');
+    }
+}
