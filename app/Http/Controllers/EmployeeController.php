79a58ffe418e3ec3aa00adb55aa636f533f363cb
Pavel Prokopenko <nipplees8@gmail.com>
2019-10-30 19:35:35 +0300

    added functions for EmployeeController and reg routes

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Department;
 use App\Employee;
+use App\Http\Requests\EmployeeRequest;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -34,9 +36,25 @@ class EmployeeController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(EmployeeRequest $request)
     {
-        //
+        $employee = new Employee([
+            'name'=>$request->get('name'),
+            'surname'=>$request->get('surname'),
+            'patronymic'=>$request->get('patronymic'),
+            'sex' =>$request->get('sex'),
+            'salary'=>$request->get('salary'),
+        ]);
+
+        $mass= $request->get('department_id');
+        $employee->save();
+        $employee=Employee::latest()->first();
+
+        self::set_department_after_create($employee, $mass);
+
+        $employee->departments()->sync($mass);
+
+        return response()->json(['success'=>true]);
     }
 
 
@@ -46,20 +64,109 @@ class EmployeeController extends Controller
     }
 
 
-    public function edit(Employee $employee)
+    public function edit(Request $request, $id)
     {
-        //
+        $mass=[];
+        $employee = Employee::findOrFail($id);
+        foreach(Employee::findOrFail($id)->departments()->get()as $item){
+            $mass[]=$item->pivot->department_id;
+        }
+
+        $employee['department_id']= $mass;
+        if($request->expectsJson()){
+            return response()->json($employee);
+        }
     }
 
 
-    public function update(Request $request, Employee $employee)
+    public function update(EmployeeRequest $request, $id)
     {
-        //
+        $employee = Employee::findOrFail($id);
+        $employee->name=$request->get('name');
+        $employee->surname=$request->get('surname');
+        $employee->patronymic=$request->get('patronymic');
+        $employee->sex =$request->get('sex');
+        $employee->salary=$request->get('salary');
+
+        $old_department_id=[];
+        foreach($employee->findOrFail($id)->departments()->get()as $item){
+            $old_department_id[]=$item->pivot->department_id;
+        }
+
+        $employee->save();
+        $mass= $request->get('department_id');
+        $employee->departments()->sync($mass);
+        self::set_department_after_edit($employee,$old_department_id);
+
+        return response()->json(['success'=>true]);
     }
 
 
-    public function destroy(Employee $employee)
+    public function destroy($id)
     {
-        //
+        $employee= Employee::findOrFail($id);
+
+        $department_id=[];
+        foreach($employee->departments()->get()as $item){
+            $department_id[]=$item->pivot->department_id;
+        }
+
+        $tmp= clone $employee;
+        $employee->delete();
+        self::set_department_after_delete($tmp,$department_id);
+
+        return response()->json(['success'=>true]);
+    }
+
+       /// Update department table after create/edit/delete action
+    public function set_department_after_create(Employee $employee, $department_id){
+        for($i=0;$i<count($department_id);$i++) {
+            $department = Department::find($department_id[$i]);
+            if ($employee->salary > $department->max_salary) {
+                Department::findOrFail($department_id[$i])->update([
+                    'amount' => $department->amount + 1,
+                    'max_salary' => $employee->salary,
+                ]);
+            } else {
+                Department::findOrFail($department_id[$i])->update(['amount' => $department->amount + 1]);
+            }
+        }
+    }
+
+    public function set_department_after_delete(Employee $employee, $department_id){
+        for($i=0;$i<count($department_id);$i++) {
+            $departm = Department::find($department_id[$i]);
+            if ($departm->amount == 1){
+                Department::findOrFail($department_id[$i])->update([
+                    'amount' => $departm->amount - 1,
+                    'max_salary' => 0,
+                ]);
+            } else if ($employee->salary == $departm->max_salary) {
+                $mass=[];
+                foreach ($departm->employees()->get()as $item){
+                    $mass[]=$item->pivot->employee_id;
+                }
+                $max_salary = Employee::whereIn('id',$mass)->max('salary');
+                Department::findOrFail($department_id[$i])->update([
+                    'amount' => $departm->amount - 1,
+                    'max_salary' => $max_salary,
+                ]);
+            } else {
+                Department::findOrFail($department_id[$i])->update(['amount' => $departm->amount -1]);
+            }
+        }
+    }
+
+    public function set_department_after_edit(Employee $employee, $old_department_id){
+        $new_department_id=[];
+        foreach($employee->departments()->get()as $item){
+            $new_department_id[]=$item->pivot->department_id;
+        }
+        if ($old_department_id==$new_department_id){
+            return;
+        }
+
+        self::set_department_after_delete($employee,$old_department_id);
+        self::set_department_after_create($employee,$new_department_id);
     }
 }
